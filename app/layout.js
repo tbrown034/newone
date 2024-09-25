bd7213cee802811fb00f015cfa8352ac3c089980
tbrown034 <tbrown034@gmail.com>
2024-09-25 19:43:40 -0400

    h

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-blue-200 min-h-screen flex flex-col justify-center items-center antialiased`}
       >
         {children}
       </body>
